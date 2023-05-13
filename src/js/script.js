58e0086192448cd94c41dec81f5683b15a20460a
Gabriel-Bonatti-Franco <127668312+Gabriel-Bonatti-Franco@users.noreply.github.com>
2023-05-13 16:10:10 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,51 @@
+const carousel = document.querySelector('.carousel');
+const firstImg = carousel.querySelectorAll('img')[0];
+const arrowIcons = document.querySelectorAll('.wrapper i');
+
+let isDragStart = false, prevPageX, prevScroolLeft;
+let firstImgWidth = firstImg.clientWidth + 14;
+
+function ajustarTamanhos(){
+    firstImgWidth = firstImg.clientWidth + 14;
+
+    let numero = (carousel.scrollLeft / firstImgWidth);
+    carousel.scrollLeft = (firstImgWidth * numero.toFixed(0));
+}
+
+arrowIcons.forEach(icon => {
+    icon.addEventListener('click',() =>{
+        carousel.scrollLeft += (icon.id == "left" ? -firstImgWidth : firstImgWidth);
+        
+    })
+});
+
+const dragStart = (e) => {
+    isDragStart = true;
+    prevPageX = e.pageX;
+    prevScroolLeft = carousel.scrollLeft;
+}
+
+const dragStop = () => {
+    isDragStart = false;
+    carousel.classList.remove('dragging');
+
+    ajustarTamanhos();
+}
+
+const dragging = (e) => {
+    if(!isDragStart) return;
+
+    e.preventDefault();
+    let positionDiff = e.pageX - prevPageX;
+    carousel.classList.add('dragging');
+
+    carousel.scrollLeft = prevScroolLeft - positionDiff;
+}
+
+carousel.addEventListener('mousedown', dragStart);
+carousel.addEventListener('mousemove', dragging);
+carousel.addEventListener('mouseup', dragStop);
+
+carousel.addEventListener('mouseout', dragStop);
+
+window.addEventListener('resize', ajustarTamanhos);
